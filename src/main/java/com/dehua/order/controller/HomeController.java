335abccb27443961a9996f0d44cfdd36e1949796
luhuajcdd <dehua@dehuadeMacBook-Pro.local>
2015-06-28 23:25:50 +0800

    f
    f

--- src/main/java/com/dehua/order/controller/HomeController.java
+++ src/main/java/com/dehua/order/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.dehua.order;
+package com.dehua.order.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
---
+++ src/main/java/com/dehua/order/controller/ali/AliUploadController.java
@@ -0,0 +1,8 @@
+package com.dehua.order.controller.ali;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AliUploadController {
+
+}
